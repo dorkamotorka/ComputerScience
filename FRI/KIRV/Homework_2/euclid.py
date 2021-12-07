e40caf41e6548253b8871d79cf74c32bc2d8c948
Teo Podobnik <tp4348@student.uni-lj.si>
2021-12-07 09:13:44 +0100

    Reorder to directory

--- FRI/KIRV/Homework_2/euclid.py
+++ FRI/KIRV/Homework_2/euclid.py
@@ -5,23 +5,29 @@ from random import randint
 from math import gcd
 import matplotlib.pyplot as plt
 
-def average(x1, x2):
-    return (x1 + x2)/2
+def average(times, step):
+    return sum(times)/step
 
 avg = 0
 ns = []
+timel = []
 avg_time = []
+step = 1000
+sub_step = 0
 for n in range(10, 10**5):
-    start = time()
     rand1 = randint(2**(n-1), 2**(n)-1)
     rand2 = randint(2**(n-1), 2**(n)-1)
+    start = time()
     gcd(rand1, rand2)
     end = time()
     dt = end - start
-    avg = average(dt, avg)
-    
-    ns.append(n)
-    avg_time.append(avg)
+    timel.append(dt)
+    if len(timel) > step:
+        sub_step += step
+        avg = average(timel, step)
+        ns.append(sub_step)
+        avg_time.append(avg)
+        timel = []
     print("Runs left: " + str(10**5 - n))
 
 plt.plot(ns, avg_time)
